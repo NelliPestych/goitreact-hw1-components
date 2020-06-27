37e7b42ed678649d81034fcd50532031a2b46dc2
NelliPestych <NelliPestych@bigmir.net>
2020-06-27 15:24:08 +0300

    new commit

---
+++ build/precache-manifest.90cb8f7318887675bf3dfda0a41b3eff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a4403d0cc151d3e3cdf7c36c274a39",
+    "url": "/goitreact-hw1-components/index.html"
+  },
+  {
+    "revision": "4fdcdba74fa7be2c49da",
+    "url": "/goitreact-hw1-components/static/css/main.2e6fa5bd.chunk.css"
+  },
+  {
+    "revision": "e4ae137243343ff8f003",
+    "url": "/goitreact-hw1-components/static/js/2.a018f589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goitreact-hw1-components/static/js/2.a018f589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fdcdba74fa7be2c49da",
+    "url": "/goitreact-hw1-components/static/js/main.3e9227c6.chunk.js"
+  },
+  {
+    "revision": "7094ba698c1b570a654a",
+    "url": "/goitreact-hw1-components/static/js/runtime-main.b0efb25d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goitreact-hw1-components/static/media/logo.5d5d9eef.svg"
+  }
+]);
